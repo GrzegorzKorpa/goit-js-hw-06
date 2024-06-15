d8f099bb5f098aeb2a3e95b8e54eb7c09772b6da
GrzegorzKorpa <grzegorz.korpa@post.pl>
2024-06-15 11:54:43 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,2 +1,37 @@
 'use strict';
 
+class StringBuilder {
+    #value;
+
+    constructor(value) {
+        this.#value = value;   
+    }
+    getValue() {
+        return this.#value;
+    }
+    
+    padStart(str) {  
+        this.str = str;
+        this.#value = `${this.str}${this.#value}`;
+    }
+
+    padEnd(strstr) {
+        this.strstr = strstr;
+        this.#value = `${this.#value}${this.strstr}`;  
+    }
+
+    padBoth(strstrstr) {
+        this.strstrstr = strstrstr;
+        this.#value = `${this.strstrstr}${this.#value}${this.strstrstr}`;   
+    }
+
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
